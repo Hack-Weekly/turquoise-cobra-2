ca48bfb55d38e83fd0dbf59fb89f4060a7b59365
Raghavendra Singh <raghavendrasingh989@gmail.com>
2023-03-15 21:27:50 +0530

    Set up fonts, colors and favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,39 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      calistoga: ['Calistoga', 'sans-serif'],
+      merriweatherRegular: ['Merriweather-Regular', 'serif'],
+      merriweatherBold: ['Merriweather-Bold', 'serif'],
+    },
+    extend: {
+      colors: {
+        turquoise: {
+          100: '#f0fbf8',
+          200: '#e2f7f2',
+          300: '#d3f3eb',
+          400: '#c3efe5',
+          500: '#b4ebde',
+          600: '#a3e7d8',
+          700: '#92e3d1',
+          800: '#80dfcb',
+          900: '#6cdac4',
+          1000: '#55d6be',
+        },
+        gunmetal: {
+          100: '#e6e7e7',
+          200: '#cdd0ce',
+          300: '#b5b9b7',
+          400: '#9ea3a0',
+          500: '#878d8a',
+          600: '#717874',
+          700: '#5b635f',
+          800: '#464f4b',
+          900: '#333c37',
+          1000: '#202a25',
+        },
+      },
+    },
   },
   plugins: [],
 };
