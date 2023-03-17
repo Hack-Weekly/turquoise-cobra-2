74c99da5dc4aa1103e4701020e70d2c0714f108c
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-03-18 01:14:14 +0530

    (chore):fixed type error for handlesubmit in Login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import LoginBox from './components/LoginBox';
-import { useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import LoginBox from "./components/LoginBox";
+import { useState } from "react";
 
 export default function Home() {
-
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <>
@@ -15,19 +14,19 @@ export default function Home() {
           name="description"
           content="Chat Application by team Turquoise Cobra"
         />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='bg-turquoise-200 mx-auto h-screen flex items-center justify-center'>
-        <div className='flex flex-col items-center justify-center'>
-          <h1 className="text-6xl font-calistoga mb-8">TurqChat</h1>
-          {!isLoggedIn ? <LoginBox /> : (
+      <main className="bg-turquoise-200 mx-auto h-screen flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          {!isLoggedIn ? (
+            <LoginBox />
+          ) : (
             <div>
-              <h2 className='font-merriweatherRegular'>Chat Window state test (WIP)</h2>
+              <h2 className="font-merriweatherRegular">
+                Chat Window state test (WIP)
+              </h2>
             </div>
           )}
         </div>
