9ff58852416be5d98d59dd565b46a9f467470c17
Miguel Roy Atienza <miguelroyatienza@gmail.com>
2023-03-17 16:43:48 +0800

    Added initial code for sending message to backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,31 @@ import Image from 'next/image';
 import LoginBox from './components/LoginBox';
 import { useState } from 'react';
 
-export default function Home() {
+export default function Home(props: { roomId: number, author: {id: number, name: string}}) {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [message, setMessage] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	setMessage(e.target.value);
+  }
+
+  const handleClick = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(message){
+      const messageData = {
+		"content": message,
+		"roomID": props.roomId,
+		"author": {
+			"id": props.author.id,
+            "name": props.author.name
+		},
+		"timestamp": new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+	  };
+	  //send to backend here, for now just console log
+      console.log(messageData);
+	}
+  }
 
   return (
     <>
@@ -35,8 +57,8 @@ export default function Home() {
         <p>Hi</p>
       </section>
       <form>
-        <input type="text" id="new-message" name="message" placeholder="Type your message here" />
-        <button type="submit">Enter</button>
+        <input type="text" id="new-message" name="message" placeholder="Type your message here" value={message} onChange={handleChange} />
+        <button type="submit" onClick={handleClick}>Enter</button>
       </form>
     </main>
       
