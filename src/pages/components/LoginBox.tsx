755ffe02162ba20cbd8c7e304d9185a9fe281d84
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2023-03-18 20:42:23 +0800

    (chore): minor style edits

--- src/pages/components/LoginBox.tsx
+++ src/pages/components/LoginBox.tsx
@@ -1,23 +1,44 @@
 export default function LoginBox() {
   const handleSubmit = async (e) => {
     // Stop the form from submitting and refreshing the page.
-    e.preventDefault()
-    console.log('Debug Message')
-  }
-  
-    return (
-    <div id='loginContainer' className='bg-gradient-to-b from-white to-[#f0fbf8] flex flex-col space-y-8 px-8 py-14 rounded-3xl font-merriweatherRegular'>
-        <div className="flex flex-col justify-center items-center gap-2">
-          <h2 className='text-3xl text-gunmetal-1000 font-calistoga'>Welcome to TurqChat</h2>
-          <h3 className='font-merriweatherRegular text-gunmetal-600'>Enter your details below</h3>
-        </div>
-        
-        <form id="loginForm" onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <input type="text" id="username" placeholder="Username" className="p-[20px] rounded-md border-2 active:border-dashed" />
-          <span className="flex flex-col items-center">
-            <button type="submit" className="w-2/4 bg-turquoise-500 text-gunmetal-1000 px-1 py-3 rounded-md font-merriweatherBold transition-all duration-150 hover:bg-turquoise-800">Get Started</button>
-          </span>
-        </form>
+    e.preventDefault();
+    console.log("Debug Message");
+  };
+
+  return (
+    <div
+      id="loginContainer"
+      className="bg-gradient-to-b from-white to-[#f0fbf8] flex flex-col space-y-8 px-8 py-14 rounded-3xl font-merriweatherRegular"
+    >
+      <div className="flex flex-col justify-center items-center gap-2">
+        <h2 className="text-3xl text-gunmetal-1000 font-calistoga">
+          Welcome to TurqChat
+        </h2>
+        <h3 className="font-merriweatherRegular text-gunmetal-600">
+          Enter your details below
+        </h3>
+      </div>
+
+      <form
+        id="loginForm"
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4"
+      >
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="p-[20px] rounded-md border-2 active:border-dashed"
+        />
+        <span className="flex flex-col items-center">
+          <button
+            type="submit"
+            className="w-2/4 bg-turquoise-500 text-gunmetal-1000 px-1 py-3 rounded-md font-merriweatherBold transition-all duration-150 hover:bg-turquoise-800"
+          >
+            Get Started
+          </button>
+        </span>
+      </form>
     </div>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,41 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import LoginBox from './components/LoginBox';
-import { useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import LoginBox from "./components/LoginBox";
+import { useState } from "react";
 
 export type IHome = {
-  roomId: number
+  roomId: number;
   author: {
-    id: number
-    name: string
-  }
-}
+    id: number;
+    name: string;
+  };
+};
 
 export default function Home(props: IHome) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [message, setMessage] = useState('')
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [message, setMessage] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
-  }
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const d = new Date();
-    if(message){
+    if (message) {
       const messageData = {
-        "content": message,
-        "roomID": props.roomId,
-        "author": {
-          "id": props.author.id,
-          "name": props.author.name
+        content: message,
+        roomID: props.roomId,
+        author: {
+          id: props.author.id,
+          name: props.author.name,
         },
-        "timestamp": d.getTime()
+        timestamp: d.getTime(),
       };
       //send to backend here, for now just console log
-      setMessage('');
+      setMessage("");
     }
-  }
+  };
 
   return (
     <>
@@ -45,31 +45,34 @@ export default function Home(props: IHome) {
           name="description"
           content="Chat Application by team Turquoise Cobra"
         />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='bg-turquoise-200 mx-auto h-screen flex items-center justify-center'>
-      <aside>
-        <ol>
-          <li>Jane Doe</li>
-          <li>Juan Perez</li>
-          <li>Mohammed Ahmed</li>
-        </ol>
-      </aside>
-      <section>
-        <p>Hey</p>
-        <p>Hello</p>
-        <p>Hi</p>
-      </section>
-      <form onSubmit={handleSubmit}>
-        <input type="text" id="new-message" name="message" placeholder="Type your message here" value={message} onChange={handleChange} />
-        <button type="submit">Enter</button>
-      </form>
-    </main>
-      
+      <main className="bg-turquoise-200 mx-auto h-screen flex items-center justify-center">
+        <aside>
+          <ol>
+            <li>Jane Doe</li>
+            <li>Juan Perez</li>
+            <li>Mohammed Ahmed</li>
+          </ol>
+        </aside>
+        <section>
+          <p>Hey</p>
+          <p>Hello</p>
+          <p>Hi</p>
+        </section>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="new-message"
+            name="message"
+            placeholder="Type your message here"
+            value={message}
+            onChange={handleChange}
+          />
+          <button type="submit">Enter</button>
+        </form>
+      </main>
     </>
   );
 }
