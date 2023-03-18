f9d7243b2c00e0ec3667c61113546a02bcadc783
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2023-03-18 22:36:18 +0800

    Add styles to input

--- src/pages/features/chat/ChatRoom/index.tsx
+++ src/pages/features/chat/ChatRoom/index.tsx
@@ -43,37 +43,50 @@ export const ChatRoom = (props: IChatRoom) => {
           <RoomListItem />
         </ol>
       </aside>
-      <section className="flex flex-auto flex-col iNametems-stretch bg-white">
-        <div className="flex flex-auto flex-col">
-          <ChatMessage />
-          <ChatMessage />
-          <ChatMessage />
-          <ChatMessage />
+      <section className="flex flex-auto flex-col items-stretch">
+        <div className="p-4">
+          <h1 className="font-bold text-2xl text-center">Room Name</h1>
         </div>
-        <form className="flex p-4" onSubmit={handleSubmit}>
-          <input
-            className="flex-auto px-4 py-2 border border-2 rounded-xl"
-            type="text"
-            id="new-message"
-            name="message"
-            placeholder="Type your message here"
-            value={message}
-            onChange={handleChange}
-          />
-          <button
-            className="text-teal-600 hover:text-teal-800 p-4"
-            type="submit"
-          >
-            <MdSend />
-          </button>
-        </form>
+        <div className="flex flex-auto flex-col bg-white rounded-3xl rounded-b-none overflow-hidden">
+          <div className="flex flex-auto flex-col p-8">
+            <ChatMessage />
+            <ChatMessage />
+            <ChatMessage />
+            <ChatMessage />
+          </div>
+          <form className="flex p-4" onSubmit={handleSubmit}>
+            <input
+              className="flex-auto px-4 py-2 border border-2 rounded-xl"
+              type="text"
+              id="new-message"
+              name="message"
+              placeholder="Type your message here"
+              value={message}
+              onChange={handleChange}
+            />
+            <button
+              className="text-teal-600 hover:text-teal-800 p-4"
+              type="submit"
+            >
+              <MdSend />
+            </button>
+          </form>
+        </div>
       </section>
     </main>
   );
 };
 
 const ChatMessage = () => {
-  return <p className="p-4">Hi</p>;
+  return (
+    <div className="pb-4">
+      <p>
+        <span className="font-bold">Sender Name</span>
+        <span className="pl-4 text-slate-400 text-sm">12:15 PM</span>
+      </p>
+      <p className="">Hi</p>
+    </div>
+  );
 };
 
 const RoomListItem = () => {
