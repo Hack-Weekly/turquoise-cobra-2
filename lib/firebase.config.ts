4c31f7cb71ece7d97490cc19baa96058485566e4
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2023-03-18 21:12:50 +0800

    Put firebase as env vars

--- lib/firebase.config.ts
+++ lib/firebase.config.ts
@@ -2,7 +2,7 @@
 import { initializeApp, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import firebase from "firebase/app"
+import firebase from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,27 +10,27 @@ import firebase from "firebase/app"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const initializeAppIfRequired = () => { 
+const initializeAppIfRequired = () => {
     try {
-        return getApp()
-    } catch (err) { 
-
+        return getApp();
+    } catch (err) {
         const firebaseConfig = {
-          apiKey: "",
-          authDomain: "",
-          projectId: "",
-          storageBucket: "",
-          messagingSenderId: "",
-          appId: "",
-          measurementId: ""
+            apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+            authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+            messagingSenderId:
+                process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+            appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+            measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
         };
         return initializeApp(firebaseConfig);
     }
-}
+};
 
 // Initialize Firebase
-const app = initializeAppIfRequired()
+const app = initializeAppIfRequired();
 // const analytics = getAnalytics(app);
 const auth = getAuth();
 
-export {auth}
+export { auth };
